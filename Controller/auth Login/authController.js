7212716afec4ffa87ee7597842e3257b99c8043d
Abu Taher <writerabutaher@gmail.com>
2023-11-08 16:21:58 +0600

    conflict solved

--- Controller/auth Login/authController.js
+++ Controller/auth Login/authController.js
@@ -2,110 +2,100 @@ const userloginModel = require("../../Model/userModel");
 const { hashPassword, comparePasswords } = require("../../middleware/bcrypt");
 const { createToken } = require("../../middleware/jsonwebtoken");
 
-<<<<<<<<< Temporary merge branch 1
-const userSingup = async (req,res)=>{
-    const {firstName,lastName,email,phoneNumber,countryName,address,password} = req.body;
-=========
 const userSingup = async (req, res) => {
-    const { firstName, lastName, email, phoneNumber, countryName, address, password } = req.body;
->>>>>>>>> Temporary merge branch 2
-    console.log("hello");
-    if (!firstName || !email || !password) {
-        res.status(401).send(false);
+  const {
+    firstName,
+    lastName,
+    email,
+    phoneNumber,
+    countryName,
+    address,
+    password,
+  } = req.body;
+
+  if (!firstName || !email || !password) {
+    res.status(401).send(false);
+  } else {
+    const checkEmail = await userloginModel.find({ email: email });
+    if (checkEmail == 0) {
+      try {
+        const encryptPassword = await hashPassword(password);
+        console.log(encryptPassword);
+        const smUserName = firstName.toLowerCase();
+        const adduser = await new userloginModel({
+          full_name: firstName + " " + lastName,
+          user_name: smUserName + Math.floor(1000 + Math.random() * 9000),
+          email: email,
+          phoneNumber: phoneNumber,
+          countryName: countryName,
+          address: address,
+          password: encryptPassword,
+        }).save();
+
+        const token = await createToken(email);
+
+        // res.cookie("_t", token, {
+        //     httpOnly: true,
+        //     secure: true,
+        //     sameSite: 'Strict',
+        //     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        //   });
+
+        res.status(200).send(true);
+      } catch (e) {
+        console.log(e);
+      }
     } else {
-        const checkEmail = await userloginModel.find({ email: email });
-        if (checkEmail == 0) {
-            try {
-                const encryptPassword = await hashPassword(password);
-                console.log(encryptPassword);
-                const smUserName = firstName.toLowerCase();
-                const adduser = await new userloginModel({
-                    full_name: firstName + " " + lastName,
-                    user_name: smUserName + (Math.floor(1000 + Math.random() * 9000)),
-                    email: email,
-                    phoneNumber: phoneNumber,
-                    countryName: countryName,
-                    address: address,
-                    password: encryptPassword,
-                }).save();
-
-                const token = await createToken(email);
-
-                // res.cookie("_t", token, {
-                //     httpOnly: true,
-                //     secure: true,
-                //     sameSite: 'Strict', 
-                //     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-                //   });
-
-                res.status(200).send(true);
-            }
-            catch (e) {
-                console.log(e);
-            }
-        }else{
-            res.status(404).send({"massage": "email already exit"})
-        }
-
+      res.status(404).send({ massage: "email already exit" });
     }
-}
-
-
+  }
+};
 
 const userLogin = async (req, res) => {
+  const { email, password } = req.body;
 
-<<<<<<<<< Temporary merge branch 1
-    const {email,password} = req.body;
-=========
-    const { email, password } = req.body;
->>>>>>>>> Temporary merge branch 2
+  if (email || password) {
+    try {
+      const data = await userloginModel.findOne({
+        $or: [{ user_name: email }, { email: email }],
+      });
 
-    if (email || password) {
+      //console.log(data.password);
 
-        try {
-            const data = await userloginModel.findOne({
-                $or: [
-                    { user_name: email },
-                    { email: email }
-                ]
+      if (data) {
+        const isMatch = await comparePasswords(password, data.password);
+
+        if (isMatch) {
+          //console.log('Passwords match. Authentication successful.');
+          res
+            .status(200)
+            .send({ massage: "Passwords match. Authentication successful." });
+        } else {
+          //console.log('Passwords do not match. Authentication failed.');
+          res
+            .status(404)
+            .send({
+              massage: "Passwords do not match. Authentication failed.",
             });
-
-            //console.log(data.password);
-
-            if (data) {
-                const isMatch = await comparePasswords(password, (data.password))
-
-                if (isMatch) {
-                    //console.log('Passwords match. Authentication successful.');
-                    res.status(200).send({"massage":'Passwords match. Authentication successful.'});
-                } else {
-                    //console.log('Passwords do not match. Authentication failed.');
-                    res.status(404).send({"massage":'Passwords do not match. Authentication failed.'});
-                }
-
-
-                const token = await createToken(data.email);
-                console.log(token);
-
-                // res.cookie("_t", token, {
-                //     httpOnly: true,
-                //     secure: true,
-                //     sameSite: 'Strict', 
-                //     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-                //   });
-            }
-
-        } catch (e) {
-
-            console.log(e);
         }
+
+        const token = await createToken(data.email);
+        console.log(token);
+
+        // res.cookie("_t", token, {
+        //     httpOnly: true,
+        //     secure: true,
+        //     sameSite: 'Strict',
+        //     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        //   });
+      }
+    } catch (e) {
+      console.log(e);
     }
-
-
-}
+  }
+};
 
 module.exports = {
-    userSingup,
-    userLogin,
-
-}
+  userSingup,
+  userLogin,
+};
