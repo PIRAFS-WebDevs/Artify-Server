d29b3405b09501d40302afed3e90fae75e4353d4
RaaS <gazitahsin323@gmail.com>
2023-11-11 21:25:45 +0600

    product model and cart

--- Model/productModel.js
+++ Model/productModel.js
@@ -1,23 +1,89 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    product_id:{
-        type:String,
-        require:true,
+    id: {
+        "type": "String",
+        "required": true
     },
-    product_name:{
-        type:String,
-        require:true,
+    name: {
+        "type": "String",
+        "required": true
     },
-    product_price:{
-        type:String,
-        require:true,
+    slug: {
+        "type": "String",
+        "required": true
     },
-    product_owner:{
-        type:String,
-        require:true,
+    description: {
+        "type": "String",
+    },
+    type_id: {
+        "type": "String",
+    },
+    price: {
+        "type": "String",
+        "required": true
+    },
+    shop_id: {
+        "type": "String",
+        "required": true
+    },
+    sale_price: {
+        "type": "String",
+        "required": true
+    },
+    min_price: {
+        "type": "String",
+        "required": true
+    },
+    max_price: {
+        "type": "String",
+        "required": true
+    },
+    quantity: {
+        "type": "String",
+        "required": true
+    },
+    in_stock: {
+        "type": "String",
+        "required": true
+    },
+    status: {
+        "type": "String",
+        "required": true
+    },
+    image: {
+        "type": "String",
+        "required": true
+    },
+    video: {
+        "type": "String",
+        "required": true
+    },
+    gallery: {
+        "type": "String",
+        "required": true
+    },
+    deleted_at: {
+        "type": "String",
+        "required": true
+    },
+    created_at: {
+        "type": "String",
+        "required": true
+    },
+    updated_at: {
+        "type": "String",
+        "required": true
+    },
+    author_id: {
+        "type": "String",
+        "required": true
+    },
+    manufacturer_id: {
+        "type": "String",
+        "required": true
     }
-
-},{timestamps:true})
+}
+,{timestamps:true})
 
 module.exports = mongoose.model("Product",productSchema);
