49e305c3b97ea1429fad7a16d495b9a615ef0686
Sagor Ahamed <104268039+SagorAhamed251245@users.noreply.github.com>
2023-11-12 18:19:12 +0600

    Update cartModel.js

--- Model/cartModel.js
+++ Model/cartModel.js
@@ -1,24 +1,24 @@
 const mongoose = require("mongoose");
 
 const cartSchema = new mongoose.Schema({
-    user_id:{
+  user_id: {
+    type: mongoose.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  item: [
+    {
+      id: {
         type: mongoose.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    item:[ 
-        {
-        id: {
-            type: mongoose.ObjectId,
-            ref: "Product",
-            required: true,
-          },
-        quantity: {
-          type: Number,
-          required: true,
+        ref: "Product",
+        required: true,
       },
-        }
-    ]
-})
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+});
 
-module.exports = mongoose.model("Cart",cartSchema);
+module.exports = mongoose.model("Cart", cartSchema);
