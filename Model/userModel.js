7c314edd6a47ad285c1337a85d1f2783809d0a30
Abu Taher <writerabutaher@gmail.com>
2023-11-19 11:49:19 +0600

    user auth

--- Model/userModel.js
+++ Model/userModel.js
@@ -1,41 +1,37 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-name:{
-        type:String,
-        require: true,
-        unique:true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    phoneNumber: {
+      type: String,
+    },
+    countryName: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+    password: {
+      type: String,
+    },
+    imgURL: {
+      type: String,
+    },
+    role: {
+      type: String,
+      default: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-    },
-    email:{
-        type:String,
-        require:true,
-        unique:true,
-
-    },
-    phoneNumber:{
-        type:String,
-
-    },
-    countryName:{
-        type:String,
-    },
-    address:{
-        type:String,
-    },
-    password:{
-        type:String,
-    },
-    imgURL:{
-        type:String,
-        
-    },
-    role:{
-        type:String,
-        default:"User",
-    }
-},
-{ timestamps: true }
-)
-
-module.exports= mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
--- route/userRoute/userRoute.js
+++ route/userRoute/userRoute.js
@@ -1,39 +1,37 @@
 const express = require("express");
-const { userSingup, userLogin, ChangePassword } = require("../../Controller/auth Login/authController");
+const {
+  userSignup,
+  userLogin,
+  ChangePassword,
+} = require("../../Controller/auth Login/authController");
 const { userCheckToken } = require("../../middleware/jsonwebtoken");
 const { cart } = require("../../Controller/cartController");
-const { ProductShowForUser, SingleProduct, BuyProduct } = require("../../Controller/product/productController");
+const {
+  ProductShowForUser,
+  SingleProduct,
+  BuyProduct,
+} = require("../../Controller/product/productController");
 const { getCategory } = require("../../Controller/categoriesController");
 const Router = express.Router();
 
 //User Login Route/API
-Router.post("/signup",userSingup);
+Router.post("/signup", userSignup);
 // Router.post("/login",userLogin);
 
-
 //User Change Password Route/API:-
-Router.post("/user/change-pass",ChangePassword)
-
+Router.post("/user/change-pass", ChangePassword);
 
 //Cart Product Route/API:-
-Router.post("/user/cart",cart);
+Router.post("/user/cart", cart);
 //User Proced to pay Route:-
-Router.get("/user/proced-to-pay",BuyProduct);
-
+Router.get("/user/proced-to-pay", BuyProduct);
 
 //Signle Product Route/API:-
-Router.get("/product/:prod_id",SingleProduct);
+Router.get("/product/:prod_id", SingleProduct);
 //All Product Route/API:-
-Router.get("/user/all-product",ProductShowForUser);
-
+Router.get("/user/all-product", ProductShowForUser);
 
 //All category show Route:-
-Router.get("/all-category",getCategory);
+Router.get("/all-category", getCategory);
 
-
-
-
-
-
-
-module.exports =Router;
+module.exports = Router;
