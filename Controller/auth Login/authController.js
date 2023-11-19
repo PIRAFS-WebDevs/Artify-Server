a931b5fb129631841ad8b79fe4783c990a2aaaa3
Tahsin <gazitahin323@gmail.com>
2023-11-19 10:53:25 +0600

    user update

--- Controller/auth Login/authController.js
+++ Controller/auth Login/authController.js
@@ -5,33 +5,26 @@ const { createToken } = require("../../middleware/jsonwebtoken");
 
 const userSingup = async (req, res) => {
   const {
-    firstName,
-    lastName,
+    name,
     email,
-    phoneNumber,
-    countryName,
-    address,
-    password,
+    photoURL,
   } = req.body;
 
-  if (!firstName || !email || !password) {
+  if (!name || !email) {
     res.status(401).send(false);
   } else {
-    const checkEmail = await userloginModel.find({ email: email });
-    if (checkEmail == 0) {
-      try {
-        const encryptPassword = await hashPassword(password);
-        console.log(encryptPassword);
-        const smUserName = firstName.toLowerCase();
-        const adduser = await new userloginModel({
-          full_name: firstName + " " + lastName,
-          user_name: smUserName + Math.floor(1000 + Math.random() * 9000),
-          email: email,
-          phoneNumber: phoneNumber,
-          countryName: countryName,
-          address: address,
-          password: encryptPassword,
-        }).save();
+    let checkEmail = await userloginModel.find({ email: email });
+
+    //let user = await userModel.findOne({ email });
+    if (!checkEmail) {
+      try{
+      user = await new userModel({
+        name: name,
+        email: email,
+        imgURL: photoURL,
+  
+      }).save();
+    
 
         const token = await createToken(email);
 
@@ -41,8 +34,8 @@ const userSingup = async (req, res) => {
         //     sameSite: 'Strict',
         //     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
         //   });
-
         res.status(200).send(true);
+      
       } catch (e) {
         console.log(e);
       }
@@ -52,49 +45,49 @@ const userSingup = async (req, res) => {
   }
 };
 
-const userLogin = async (req, res) => {
-  const { email, password } = req.body;
+// const userLogin = async (req, res) => {
+//   const { email, password } = req.body;
 
-  if (email || password) {
-    try {
-      const data = await userloginModel.findOne({
-        $or: [{ user_name: email }, { email: email }],
-      });
+//   if (email || password) {
+//     try {
+//       const data = await userloginModel.findOne({
+//         $or: [{ user_name: email }, { email: email }],
+//       });
 
-      //console.log(data.password);
+//       //console.log(data.password);
 
-      if (data) {
-        const isMatch = await comparePasswords(password, data.password);
+//       if (data) {
+//         const isMatch = await comparePasswords(password, data.password);
 
-        if (isMatch) {
-          //console.log('Passwords match. Authentication successful.');
-          res
-            .status(200)
-            .send({ massage: "Passwords match. Authentication successful." });
-        } else {
-          //console.log('Passwords do not match. Authentication failed.');
-          res
-            .status(404)
-            .send({
-              massage: "Passwords do not match. Authentication failed.",
-            });
-        }
+//         if (isMatch) {
+//           //console.log('Passwords match. Authentication successful.');
+//           res
+//             .status(200)
+//             .send({ massage: "Passwords match. Authentication successful." });
+//         } else {
+//           //console.log('Passwords do not match. Authentication failed.');
+//           res
+//             .status(404)
+//             .send({
+//               massage: "Passwords do not match. Authentication failed.",
+//             });
+//         }
 
-        const token = await createToken(data.email);
-        console.log(token);
+//         const token = await createToken(data.email);
+//         console.log(token);
 
-        // res.cookie("_t", token, {
-        //     httpOnly: true,
-        //     secure: true,
-        //     sameSite: 'Strict',
-        //     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-        //   });
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  }
-};
+//         // res.cookie("_t", token, {
+//         //     httpOnly: true,
+//         //     secure: true,
+//         //     sameSite: 'Strict',
+//         //     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+//         //   });
+//       }
+//     } catch (e) {
+//       console.log(e);
+//     }
+//   }
+// };
 
 const ChangePassword = async (req, res) => {
   try {
