e7ed8b78cf124bf3268880a7a84f582523a5b952
Abu Taher <writerabutaher@gmail.com>
2023-11-19 18:21:48 +0600

    Update tagsController.js

--- Controller/tagsController.js
+++ Controller/tagsController.js
@@ -1,80 +1,82 @@
 const tagsModel = require("../Model/tagsModel");
 
-const tags = async (req,res)=>{
+const tags = async (req, res) => {
+  try {
+    const { name, slug, details } = req.body;
     try {
-        const {name,slug, details} = req.body;
-        try {
-            const checkdata = await tagsModel.find({name:name});
-            if(!checkdata){
-                await tagsModel({
-                    name,
-                    slug, details,
-                }).save();
-                res.status(200).send({success: true,massage:"tags Uploaded"})
-            }else{
-                res.status(404).send({success: false,massage:"tags all ready exists"})
-            }
-        } catch (error) {
-            res.status(500).send({success: false,massage:"Internal server error"});
-            
-        }
-    } catch (error) {
-        res.status(501).send({success: false,massage:"fatching value error"})
-        
-    }
-}
-
-const updatetags = async(req,res)=>{
-    try {
-        const {_id,name,slug, details} = req.body;
-        if(!id || !name || !slug || !details){
-            res.status(401).send({success: false,massage:"fill proper data"})
-        }else{
-
-            const checkdata = await tagsModel.findOne({_id:_id});
-            if(checkdata){
-                await tagsModel.findByIdAndUpdate(_id,{name,slug, details},{new:true});
-                res.status(200).send({success: true,massage:"update succesfully"});
-            }else{
-                res.status(404).send({success:false,massage:"tags not found"});
-            }
-        }
-
-    } catch (error) {
-        res.status(500).send({success: false,massage:"internal server error"});
-
-        
-    }
-}
-
-const gettags = async (req, res)=>{
-    try {
-        const data = await tagsModel.find();
-        res.status(200).send({success:true,data});
-
-    } catch (error) {
-        res.status(500).send({success:false,massage:"internal server error"});
-    }
-}
-
-const tagsDelete = async (req,res)=>{
-    try {
-      const {_id}=req.body;
-      const checkdata = await tagsModel.findOne(_id);
-      if(checkdata){
-        await tagsModel.findByIdAndRemove(_id);
-        res.status(200).send({success:true});
-      }else{
-        res.status(404).send({success:false,massage:"tags not found"});
+      const checkdata = await tagsModel.findOne({ name: name });
+      if (!checkdata) {
+        await tagsModel({
+          name,
+          slug,
+          details,
+        }).save();
+        res.status(200).send({ success: true, massage: "tags Uploaded" });
+      } else {
+        res
+          .status(404)
+          .send({ success: false, massage: "tags all ready exists" });
       }
     } catch (error) {
-      res.status(500).send({success:false,massage:"internal server error"});
+      res
+        .status(500)
+        .send({ success: false, massage: "Internal server error" });
     }
+  } catch (error) {
+    res.status(501).send({ success: false, massage: "fatching value error" });
   }
+};
+
+const updatetags = async (req, res) => {
+  try {
+    const { _id, name, slug, details } = req.body;
+    if (!id || !name || !slug || !details) {
+      res.status(401).send({ success: false, massage: "fill proper data" });
+    } else {
+      const checkdata = await tagsModel.findOne({ _id: _id });
+      if (checkdata) {
+        await tagsModel.findByIdAndUpdate(
+          _id,
+          { name, slug, details },
+          { new: true }
+        );
+        res.status(200).send({ success: true, massage: "update succesfully" });
+      } else {
+        res.status(404).send({ success: false, massage: "tags not found" });
+      }
+    }
+  } catch (error) {
+    res.status(500).send({ success: false, massage: "internal server error" });
+  }
+};
+
+const gettags = async (req, res) => {
+  try {
+    const data = await tagsModel.find();
+    res.status(200).send({ success: true, data });
+  } catch (error) {
+    res.status(500).send({ success: false, massage: "internal server error" });
+  }
+};
+
+const tagsDelete = async (req, res) => {
+  try {
+    const { _id } = req.body;
+    const checkdata = await tagsModel.findOne(_id);
+    if (checkdata) {
+      await tagsModel.findByIdAndRemove(_id);
+      res.status(200).send({ success: true });
+    } else {
+      res.status(404).send({ success: false, massage: "tags not found" });
+    }
+  } catch (error) {
+    res.status(500).send({ success: false, massage: "internal server error" });
+  }
+};
 
 module.exports = {
-tags,
-updatetags,
-gettags,
-tagsDelete
-}
+  tags,
+  updatetags,
+  gettags,
+  tagsDelete,
+};
