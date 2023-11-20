5091b5037325ee2cc499b43cd2fe04e3ac82a6b6
Abu Taher <writerabutaher@gmail.com>
2023-11-20 11:36:39 +0600

    Update layoutController.js

--- Controller/layoutController.js
+++ Controller/layoutController.js
@@ -1,81 +1,82 @@
 const layoutModel = require("../Model/layoutModel");
 
-const layout = async (req,res)=>{
+const layout = async (req, res) => {
+  try {
+    const { name, slug, details } = req.body;
     try {
-        const {name,slug, details} = req.body;
-        try {
-            const checkdata = await layoutModel.find({name:name});
-            if(!checkdata){
-                await layoutModel({
-                    name,
-                    slug, details,
-                }).save();
-                res.status(200).send({success: true,massage:"layout Uploaded"})
-            }else{
-                res.status(404).send({success: false,massage:"layout all ready exists"})
-            }
-        } catch (error) {
-            res.status(500).send({success: false,massage:"Internal server error"});
-            
-        }
-    } catch (error) {
-        res.status(501).send({success: false,massage:"fatching value error"})
-        
-    }
-}
-
-const updatelayout = async(req,res)=>{
-    try {
-        const {_id,name,slug, details} = req.body;
-        if(!id || !name || !slug || !details){
-            res.status(401).send({success: false,massage:"fill proper data"})
-        }else{
-
-            const checkdata = await layoutModel.findOne({_id:_id});
-            if(checkdata){
-                await layoutModel.findByIdAndUpdate(_id,{name,slug, details},{new:true});
-                res.status(200).send({success: true,massage:"update succesfully"});
-            }else{
-                res.status(404).send({success:false,massage:"layout not found"});
-            }
-        }
-
-    } catch (error) {
-        res.status(500).send({success: false,massage:"internal server error"});
-
-        
-    }
-}
-
-const getlayout = async (req, res)=>{
-    try {
-        const data = await layoutModel.find();
-        res.status(200).send({success:true,data});
-
-    } catch (error) {
-        res.status(500).send({success:false,massage:"internal server error"});
-        
-    }
-}
-
-const layoutDelete = async (req,res)=>{
-    try {
-      const {_id}=req.body;
-      const checkdata = await layoutModel.findOne(_id);
-      if(checkdata){
-        await layoutModel.findByIdAndRemove(_id);
-        res.status(200).send({success:true});
-      }else{
-        res.status(404).send({success:false,massage:"layout not found"});
+      const checkdata = await layoutModel.findOne({ name: name });
+      if (!checkdata) {
+        await layoutModel({
+          name,
+          slug,
+          details,
+        }).save();
+        res.status(200).send({ success: true, massage: "layout Uploaded" });
+      } else {
+        res
+          .status(404)
+          .send({ success: false, massage: "layout all ready exists" });
       }
     } catch (error) {
-      res.status(404).send({success:false,massage:"internal server error"});
+      res
+        .status(500)
+        .send({ success: false, massage: "Internal server error" });
     }
+  } catch (error) {
+    res.status(501).send({ success: false, massage: "fatching value error" });
   }
+};
+
+const updatelayout = async (req, res) => {
+  try {
+    const { _id, name, slug, details } = req.body;
+    if (!id || !name || !slug || !details) {
+      res.status(401).send({ success: false, massage: "fill proper data" });
+    } else {
+      const checkdata = await layoutModel.findOne({ _id: _id });
+      if (checkdata) {
+        await layoutModel.findByIdAndUpdate(
+          _id,
+          { name, slug, details },
+          { new: true }
+        );
+        res.status(200).send({ success: true, massage: "update succesfully" });
+      } else {
+        res.status(404).send({ success: false, massage: "layout not found" });
+      }
+    }
+  } catch (error) {
+    res.status(500).send({ success: false, massage: "internal server error" });
+  }
+};
+
+const getlayout = async (req, res) => {
+  try {
+    const data = await layoutModel.find();
+    res.status(200).send({ success: true, data });
+  } catch (error) {
+    res.status(500).send({ success: false, massage: "internal server error" });
+  }
+};
+
+const layoutDelete = async (req, res) => {
+  try {
+    const { _id } = req.body;
+    const checkdata = await layoutModel.findOne(_id);
+    if (checkdata) {
+      await layoutModel.findByIdAndRemove(_id);
+      res.status(200).send({ success: true });
+    } else {
+      res.status(404).send({ success: false, massage: "layout not found" });
+    }
+  } catch (error) {
+    res.status(404).send({ success: false, massage: "internal server error" });
+  }
+};
 
 module.exports = {
-layout,
-updatelayout,
-getlayout,
-layoutDelete
-}
+  layout,
+  updatelayout,
+  getlayout,
+  layoutDelete,
+};
