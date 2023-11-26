51f0825bac5a6e6275975724c3cac1b6f5acbd62
Abu Taher <writerabutaher@gmail.com>
2023-11-26 15:02:51 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,53 +2,43 @@ const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const connectDatabase = require("./config/database")
+const connectDatabase = require("./config/database");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-
 
 const userRoute = require("./route/userRoute/userRoute");
 const adminRoute = require("./route/adminRoute/adminRoute");
 
-
-
-dotenv.config({path:"./config.env"})
+dotenv.config({ path: "./config.env" });
 const port = process.env.PORT || 8080;
 
 connectDatabase();
 
 const corsOptions = {
-    origin: ['http://localhost:3000'],
-    credentials: true,
-  };
-
-  
-
-
-
+  origin: ["http://localhost:3000", "https://waresun-client-nine.vercel.app/"],
+  credentials: true,
+};
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', corsOptions.origin);
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-  });
-  
+  res.header("Access-Control-Allow-Origin", corsOptions.origin);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use("/api/v1/auth",userRoute);
-app.use("/api/v1/auth",adminRoute);
+app.use("/api/v1/auth", userRoute);
+app.use("/api/v1/auth", adminRoute);
 app.use(morgan("dev"));
 app.use(cookieParser());
 
-
-
-
-
-
-app.get('/', (req, res) => {
-    res.send('gogoshop server is running');
-})
-app.listen(port,()=>{
-    console.log(`http://localhost:${port}`)
-})
+app.get("/", (req, res) => {
+  res.send("gogoshop server is running");
+});
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
