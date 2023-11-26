050f75f2c4a896f5b10022b7a7de63effd5667d3
Tahsin <gazitahin323@gmail.com>
2023-11-26 20:03:37 +0600

    dd

--- Model/cartModel.js
+++ Model/cartModel.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 const cartSchema = new mongoose.Schema({
   user_id: {
-    type: mongoose.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true,
   },
   item: [
     {
       id: {
-        type: mongoose.ObjectId,
+        type: mongoose.Schema.Types.ObjectId,
         ref: "Product",
         required: true,
       },
